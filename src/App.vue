1ddf831b1a7172ee02c401028538598474dcebd4
pete20020 <68525430+pete20020@users.noreply.github.com>
2020-08-24 18:22:41 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+ <div id="app" >
+    <HelloWorld msg="LIKE-UNLIKE" /><hr><br>
+    <h1>{{total}}</h1>
+    <div class="row">
+      <div class="col-12">
+        <img alt="Vue logo" src="./assets/1.jpg" width="490" /><br><br><hr>
+      </div>
+      <div class="col-12">
+        <CsButton @cc="sum" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue"
+import CsButton from "./components/CsButton.vue"
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    CsButton,
+  },
+  data: function(){
+    return {
+      total: 0,
+    };
+  },
+  methods: {
+    sum(counterValue) {
+      this.total = counterValue;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CsButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button v-on:click="upFunction" class="btn btn-info mr-3" >{{lable}}LIKE</button>
+    <button v-on:click="downFunction" class="btn btn-info mr-3" >{{lable}}LOVE</button>
+    <button v-on:click="LoveFunction" class="btn btn-info mr-3">{{lable}}UNLIKE</button>
+    <button v-on:click="ResetFunction" class="btn btn-info mr-4">{{lable}}RESET</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Soda",
+  props: {
+    label: String,
+  },
+  data: function () {
+    return {
+      counter: 0,
+      fsize : 50 ,
+    };
+  },
+  
+  methods : {
+    upFunction : function(){
+      this.counter += 1
+      this.$emit('cc',this.counter)
+    },
+    downFunction : function(){
+      this.counter += 5
+      this.$emit('cc',this.counter)
+    },
+    LoveFunction: function () {
+      this.counter -= 10
+      this.$emit("cc", this.counter);
+    },
+    ResetFunction: function () {
+      this.counter = 0
+      this.$emit("cc", this.counter);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
